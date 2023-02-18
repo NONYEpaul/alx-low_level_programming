c4104dcc2287663e74003acf14b73b65ba17b73b
NONYEpaul <helenonyepaul@gmail.com>
2023-02-17 17:58:43 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main - prints all single digit numbers of base 9 starting from 0
  *Return: 0
@@ -8,8 +9,7 @@ int main(void)
 	int n;
 
 	n = 0;
-	
-	for (; n = 0, n <= 9, n++;)
+	for (; n < 10; n++)
 
 		printf("%d", n);
 
