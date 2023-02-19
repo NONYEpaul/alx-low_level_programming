f0a1dc9ae93dcddab61cca686f038f6f31c7cf20
NONYEpaul <helenonyepaul@gmail.com>
2023-02-19 08:22:52 -0800

    The success combination in business is: Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,19 +5,24 @@
  */
 int main(void)
 {
-	int f1, f2;
+	int f1, f2, f3;
 
-	for (f1 = 0; f1 < 9; f1++)
+	for (f1 = 0; f1 < 8; f1++)
 	{
-		for (f2 = f1 + 1; f2 < 10; f2++)
+		for (f2 = f1 + 1; f2 < 9; f2++)
 		{
+			for (f3 = f2 + 1; f3 < 10; f3++)
+			{
 			putchar((f1 % 10) + '0');
 			putchar((f2 % 10) + '0');
-			if (f1 == 8 && f2 == 9)
+			putchar((f3 % 10) + '0');
+
+			if (f1 == 7 && f2 == 8 && f3 == 9)
 				continue;
 
 			putchar(',');
 			putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
