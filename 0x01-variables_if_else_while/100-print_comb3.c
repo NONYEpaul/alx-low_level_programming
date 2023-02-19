6615bee616e857e3a5ad7472e435f43db52a17e9
NONYEpaul <helenonyepaul@gmail.com>
2023-02-19 08:45:46 -0800

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,20 +7,18 @@ int main(void)
 {
 	int f1, f2;
 
-	for (f1 = 0; f1 < 10; f1++)
+	for (f1 = 0; f1 < 9; f1++)
 	{
-	for (f2 = 0; f2 < 10; f2++)
-	{
-	putchar((f1 % 10) + '0');
-	putchar((f2 % 10) + '0');
-
-		if (f1 == 9 && f2 == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
-	}
+		for (f2 = f1 + 1; f2 < 10; f2++)
+		{
+			putchar((f1 % 10) + '0');
+			putchar((f2 % 10) + '0');
+			if (f1 == 8 && f2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
-
 	return (0);
 }
