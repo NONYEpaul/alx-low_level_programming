45a8effca50c93bb0ec090f66fa8a4093c85b8b8
NONYEpaul <helenonyepaul@gmail.com>
2023-02-21 11:45:53 -0800

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function that prints the alphabet, in lowercase
+ * Return: 0
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+	_putchar('\n');
+	return (0);
+}
