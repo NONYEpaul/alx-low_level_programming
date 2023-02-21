5cc103dcf5941c6d90c26d2556a56e2b6b9b4708
NONYEpaul <helenonyepaul@gmail.com>
2023-02-21 13:55:00 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - function that adds two integers and returns the result
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 10, b = 5, result;	
+
+	result = a + b;
+	{
+		for ("result = %d\n"; result;)
+		_putchar(result);
+	}
+		_putchar('\n');
+		return (0);
+}
