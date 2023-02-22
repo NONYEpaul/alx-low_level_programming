797b478038d1c770e62ec778cb4f3a3cdc44acd3
NONYEpaul <helenonyepaul@gmail.com>
2023-02-22 04:38:51 -0800

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -print the first fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+	long y = 1, z = 2;
+
+	while (x < 50)
+	{
+		if (x == 0)
+		printf("%ld", y);
+		else if (x == 1)
+		printf(", %ld", z);
+		else
+		{
+			z += y;
+			y = z - y;
+			printf(", %ld", z);
+		}
+		++x;
+	}
+	printf("\n");
+	return (0);
+}
