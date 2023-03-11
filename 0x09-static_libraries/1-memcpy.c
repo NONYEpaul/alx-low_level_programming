464a38ce72d270fcbb32759d923e16bfa876fa86
NONYEpaul <helenonyepaul@gmail.com>
2023-03-11 07:11:37 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - function copies n bytes from memory area
+ * @dest: where memory is stored
+ * @src: where memory is copied
+ * @n: bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int w = 0;
+	int p = n;
+
+	for (; w < p; w++)
+	{
+		dest[w] = src[w];
+		n--;
+	}
+	return (dest);
+}
